16b5b860cbf89928afe2c2fdc810cbab463d0460
HvrlK <g.v.s.23.07@gmail.com>
2018-08-13 14:42:59 +0300

    Create UI.

--- MyMenu/AppDelegate.swift
+++ MyMenu/AppDelegate.swift
@@ -11,38 +11,13 @@ import Firebase
 import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-   
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            print("Failed to log into Google", error)
-            return
-        }
-        print("Successfully logged into Google", user)
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
-            if let error = error {
-                print("Log in failed", error)
-                return
-            }
-        }
-    }
-    
-    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(
-            url,
-            sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
-            annotation: [:]
-        )
-    }
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
+        
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().clientID = "630029746458-nk9bnu1er9oflagtre6k9llfn5li42m9.apps.googleusercontent.com"
         return true
     }
 
--- Podfile
+++ Podfile
@@ -1,4 +1,6 @@
+use_frameworks!
 target "MyMenu" do
+    pod 'GoogleAPIClientForREST/Sheets'
     pod 'GoogleSignIn'
     pod 'Firebase/Core'
     pod 'Firebase/Auth'
